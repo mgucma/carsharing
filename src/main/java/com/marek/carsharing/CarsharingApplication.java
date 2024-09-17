efc26c41a55b3cb0db232d1e966c2e004f45e24b
mgucma <marek.gucma@gmail.com>
2024-09-17 15:33:12 +0200

    ci (#1)

--- src/main/java/com/marek/carsharing/CarsharingApplication.java
+++ src/main/java/com/marek/carsharing/CarsharingApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CarsharingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CarsharingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CarsharingApplication.class, args);
+    }
 
 }
