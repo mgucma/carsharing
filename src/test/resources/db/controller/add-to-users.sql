9aab12b3e0a96b5d1dd790741c4cc720fe00b4ef
mgucma <marek.gucma@gmail.com>
2024-10-03 09:27:44 +0200

    app fix sql 2

--- src/test/resources/db/controller/add-to-users.sql
+++ src/test/resources/db/controller/add-to-users.sql
@@ -1,2 +1,2 @@
 INSERT INTO users (id, email, first_name, last_name, password, role, deleted)
-VALUES(1, 'admin@simpleart.eu', 'Admin', 'User', 'password', 'MANAGER', false);
+VALUES (1, 'admin@simpleart.eu', 'B', 'C', 'password', 'MANAGER', false);
